7bca4589df9c53a02d6b4f947f8f909e76fd84f4
Ayman M. Ahmed <evilayman@gmail.com>
2018-04-19 11:43:59 +0200

    Basic Scene

---
+++ Assets/Scripts/ApplyForce.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplyForce : MonoBehaviour
+{
+    private Rigidbody playerRB;
+
+    public Transform LeftHandTrigger, RightHandTrigger, Player;
+
+    public float HandVelocity, PlayerSpeed, TiltSpeed;
+    private float LeftVelocity, RightVelocity;
+    private Vector3 LeftHand, RightHand;
+
+    private bool isPressedLeft = false, isPressedRight = false, NothingPressed = true;
+    public bool IsPressedLeft
+    {
+        get
+        {
+            return isPressedLeft;
+        }
+
+        set
+        {
+            isPressedLeft = value;
+        }
+    }
+    public bool IsPressedRight
+    {
+        get
+        {
+            return isPressedRight;
+        }
+
+        set
+        {
+            isPressedRight = value;
+        }
+    }
+
+
+    void Start ()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+	
+	void Update ()
+    {
+        if (IsPressedLeft)
+            LeftVelocity = HandVelocity;
+        else
+            LeftVelocity = 0;
+
+        if (IsPressedRight)
+            RightVelocity = HandVelocity;
+        else
+            RightVelocity = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        //playerRB.velocity = (Player.forward * PlayerSpeed) + (new Vector3(0,-LeftHandTrigger.forward.y,0) * LeftVelocity) + (new Vector3(0, -RightHandTrigger.forward.y, 0) * RightVelocity);
+        playerRB.velocity = ((Player.forward * PlayerSpeed) + (-LeftHandTrigger.forward * LeftVelocity) + (-RightHandTrigger.forward * RightVelocity));
+        //playerRB.AddForce((Player.forward * PlayerSpeed) + (-LeftHandTrigger.forward * LeftVelocity) + (-RightHandTrigger.forward * RightVelocity));
+        Debug.Log(playerRB.velocity.z);
+
+    }
+}
---
+++ Assets/Scripts/MoveRightHand.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRightHand : MonoBehaviour
+{
+    private SteamVR_TrackedObject trackedObject;
+    private SteamVR_Controller.Device device;
+
+    ApplyForce AFScript;
+    public ParticleSystem Emission;
+
+    void Start()
+    {
+
+        trackedObject = GetComponentInParent<SteamVR_TrackedObject>();
+        AFScript = GameObject.Find("PlayerMotherParent").GetComponent<ApplyForce>();
+
+    }
+
+    void Update()
+    {
+
+        device = SteamVR_Controller.Input((int)trackedObject.index);
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            AFScript.IsPressedRight = true;
+            device.TriggerHapticPulse(1000);
+            Emission.Play();
+
+        }
+        if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            AFScript.IsPressedRight = false;
+            Emission.Stop();
+
+        }
+    }
+}
