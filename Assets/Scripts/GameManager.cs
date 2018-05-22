95a1efc04d429517328d82fb99d6ae2f6abd3369
Ayman M.Ahmed <33355388+evilayman@users.noreply.github.com>
2018-05-22 02:35:42 +0200

    player ranks, start timer

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-
     public enum GameState
     {
         none,
@@ -15,6 +15,45 @@ public class GameManager : MonoBehaviour
 
     public GameState currentState;
 
+    public float countDownTime, timerCD;
+    public int fontSize, maxFontSize, fontSizeIncrementSpeed;
+
+    public TextMeshPro countDownTimeText;
+    public Transform finishLine;
+
+    private CooldownTimer countDownTimer;
+    private List<GameObject> myPlayersSorted;
+
+    public List<GameObject> MyPlayersSorted
+    {
+        get
+        {
+            return myPlayersSorted;
+        }
+
+        set
+        {
+            myPlayersSorted = value;
+        }
+    }
+
+    private void Start()
+    {
+        countDownTimer = new CooldownTimer(timerCD, false);
+        MyPlayersSorted = new List<GameObject>();
+        AddPlayers();
+
+    }
+
+    void AddPlayers()
+    {
+        var players = GameObject.FindGameObjectsWithTag("PlayerParent");
+        for (int i = 0; i < players.Length; i++)
+        {
+            MyPlayersSorted.Add(players[i]);
+        }
+    }
+
     private void Update()
     {
         switch (currentState)
@@ -22,8 +61,10 @@ public class GameManager : MonoBehaviour
             case GameState.none:
                 break;
             case GameState.preGame:
+                CountDown();
                 break;
             case GameState.inGame:
+                RankPlayers();
                 break;
             case GameState.endGame:
                 break;
@@ -31,4 +72,49 @@ public class GameManager : MonoBehaviour
                 break;
         }
     }
+
+    private void CountDown()
+    {
+        if (countDownTime > -2)
+        {
+            if (countDownTime <= 0)
+            {
+                countDownTimeText.text = "Go Noobs!";
+            }
+            else
+            {
+                countDownTimeText.text = countDownTime.ToString();
+            }
+
+            if (countDownTime <= 3 && countDownTimeText.fontSize < maxFontSize)
+            {
+                countDownTimeText.fontSize += fontSizeIncrementSpeed;
+            }
+
+            if (countDownTimer.IsReady())
+            {
+                countDownTimer.Reset();
+                countDownTime -= timerCD;
+
+                if(countDownTime >= 0)
+                    countDownTimeText.fontSize = fontSize;
+            }
+        }
+        else
+        {
+            countDownTimeText.gameObject.SetActive(false);
+            currentState = GameState.inGame;
+        }
+    }
+
+    private void RankPlayers()
+    {
+        MyPlayersSorted.Sort(SortByDistance);
+    }
+
+    private int SortByDistance(GameObject A, GameObject B)
+    {
+        return -(A.transform.position.z - finishLine.position.z).CompareTo(B.transform.position.z - finishLine.position.z);
+    }
+
 }
--- Assets/Scripts/Network/PlayerManagerForNetwork.cs
+++ Assets/Scripts/Network/PlayerManagerForNetwork.cs
@@ -6,12 +6,12 @@ using TMPro;
 
 public class PlayerManagerForNetwork : MonoBehaviour
 {
+    private GameManager GM;
     public Stats myStats;
     public GameObject leftHand, rightHand;
-    public TextMeshPro playerName;
+    public TextMeshPro playerName, playerRank;
 
     private PhotonView photonView;
-
     private bool inBoostRegion, Died = false;
 
     public bool InBoostRegion
@@ -30,30 +30,38 @@ public class PlayerManagerForNetwork : MonoBehaviour
     private void Start()
     {
         photonView = GetComponent<PhotonView>();
+        GM = GameObject.Find("GameManager").GetComponent<GameManager>();
 
         if (photonView.isMine)
         {
-            FadeFromBlack(0.2f);
+            FadeFromBlack(0.5f);
         }
 
         if (!photonView.isMine)
         {
             playerName.SetText(photonView.owner.NickName);
         }
-
     }
 
     private void Update()
     {
         if (!photonView.isMine)
         {
+            setRankText();
             rotateText();
         }
     }
 
     private void rotateText()
     {
-        playerName.transform.rotation = Camera.main.transform.rotation;
+        if(Camera.main)
+            playerName.transform.rotation = playerRank.transform.rotation = Camera.main.transform.rotation;
+    }
+
+    private void setRankText()
+    {
+        var i = GM.MyPlayersSorted.FindIndex(x => x == gameObject);
+        playerRank.SetText(GetRankString(i));
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -76,7 +84,6 @@ public class PlayerManagerForNetwork : MonoBehaviour
             if (other.gameObject.name == "BoostRegion")
             {
                 InBoostRegion = true;
-
             }
         }
     }
@@ -113,6 +120,27 @@ public class PlayerManagerForNetwork : MonoBehaviour
         yield return new WaitForSeconds(1f);
         this.transform.position = new Vector3(0, 50, 0);
         Died = false;
+    }
+
+    private string GetRankString(int index)
+    {
+        string rank;
+        switch (index)
+        {
+            case 0:
+                rank = "1st";
+                break;
+            case 1:
+                rank = "2nd";
+                break;
+            case 2:
+                rank = "3rd";
+                break;
+            default:
+                rank = "";
+                break;
+        }
+        return rank;
     }
 
 }
