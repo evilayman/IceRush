49293475914a8b8b866a760c35846af4e23fa431
AbdElrahman Atwan <abdelrahman_atwan@hotmail.com>
2018-05-24 10:05:11 +0200

    Buildings

---
+++ Assets/Scripts/TunnelMovment.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TunnelMovment : MonoBehaviour
+{
+    public float force;
+    private Collider col;
+    // Use this for initialization
+    void Start()
+    {
+        if (gameObject.GetComponent<BoxCollider>() == null)
+            col = gameObject.AddComponent<BoxCollider>();
+
+        col = gameObject.GetComponent<BoxCollider>();
+        col.isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.forward = gameObject.transform.forward;
+            
+                
+        }
+    }
+}
