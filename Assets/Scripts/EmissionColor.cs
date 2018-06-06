f5e1ba2745f53fe239783f0fb75fe9d3f63a5f13
Ayman M.Ahmed <33355388+evilayman@users.noreply.github.com>
2018-06-06 03:19:26 +0200

    emission color

---
+++ Assets/Scripts/EmissionColor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmissionColor : MonoBehaviour
+{
+    public Color emissionColor;
+    public List<Material> myMats;
+    private Color tempColor;
+
+    void Update()
+    {
+        if(emissionColor != tempColor)
+        {
+            tempColor = emissionColor;
+            for (int i = 0; i < myMats.Count; i++)
+            {
+                myMats[i].SetColor("_EmissionColor", emissionColor);
+            }
+        }
+    }
+}
