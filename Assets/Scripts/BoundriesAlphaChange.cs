1a0b003380595c2607b48e1549c7e71967b8b467
Ahmed Mohamed Abd El-Hafez <ahmedmabdelhafez@hotmail.com>
2018-06-06 10:29:48 +0200

    1k level--particle systems now doesn't leak into game region--slight adjust to particle systems

---
+++ Assets/Scripts/BoundriesAlphaChange.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundriesAlphaChange : MonoBehaviour
+{
+
+    [SerializeField]
+    private Material material;
+    double alpha;
+    Color temp;
+    bool turnAlphaDown;
+    void Start()
+    {
+        material = gameObject.GetComponent<Renderer>().material;
+        alpha = 0;
+    }
+
+
+    private void FixedUpdate()
+    {
+        if (alpha < 1 && !turnAlphaDown)
+        {
+            alpha += 0.01;
+            temp = material.color;
+            temp.a += (float)0.01;
+            material.color = temp;
+        }
+        else
+        {
+            turnAlphaDown = true;
+            alpha -= 0.01;
+            temp = material.color;
+            temp.a -= (float)0.01;
+            material.color = temp;
+            if (alpha==0)
+            {
+                turnAlphaDown = false;
+            }
+        }
+
+    }
+}
